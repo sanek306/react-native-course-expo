7b150425904d06d59cfc8aba289fa158aec386a3
sanek306 <sanek306@gmail.com>
2021-09-21 23:17:05 +0300

    autoCapitalize autoCorrect

--- src/AddTodo.js
+++ src/AddTodo.js
@@ -21,6 +21,8 @@ export const AddTodo = ({ onSubmit }) => {
                 onChangeText={setValue}
                 value={value}
                 placeholder="Введите название дела..."
+                autoCorrect={false}
+                autoCapitalize='none'
             />
             <Button 
                 title="Добавить"
