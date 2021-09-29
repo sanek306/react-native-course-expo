99c1f3b345bee028698f9a33854b4549e8adc00e
sanek306 <sanek306@gmail.com>
2021-09-30 01:11:13 +0300

    added ui wrapper

--- src/screens/TodoScreen.js
+++ src/screens/TodoScreen.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import { View, StyleSheet, Text, Button } from 'react-native';
+import { AppCard } from '../components/ui/Card';
 import { THEME } from '../theme';
 
 export const TodoScreen = ({ goBack, todo }) => {
     return (
         <View>
-            <Text>{todo && todo.title}</Text>
+            <AppCard style={styles.card}>
+                <Text style={styles.title}>{todo && todo.title}</Text>
+                <Button title='Ред.' />
+            </AppCard>
             <View style={styles.buttons}>
                 <Button title='Назад' color={THEME.DANGER_COLOR} onPress={goBack} />
                 <Button title='Удалить' color={THEME.GRAY_COLOR} onPress={() => console.log('remove')} />
@@ -15,6 +19,13 @@ export const TodoScreen = ({ goBack, todo }) => {
 }
 
 const styles = StyleSheet.create({
+    title: {
+        fontSize: 20,
+    },
+    card: {
+        marginBottom: 20,
+        padding: 15,
+    },
     buttons: {
         flexDirection: 'row',
         justifyContent: 'space-between'
